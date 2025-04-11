9f80553394d152cdc43b9decd79cd2a0b990a514
krish garg <krish1204.becse24@chitkara.edu.in>
2025-04-11 16:58:43 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,29 +1,33 @@
 #include<stdio.h>
 int main()
 {
-    int n,i;
-    scanf("%d",&n);
-    int arr[n];
-    for(i=0;i<n;i++)
+   int n,i;
+   scanf("%d",&n);
+   int arr[n];
+   for(i=0;i<n;i++)
+   {
+    printf("%d",&arr[n]);
+   }
+   for(i=0;i<n;i++)
+   {
+    if(arr[i-1]<arr[i] && arr[i]>arr[i+1])
     {
-        scanf("%d",&arr[i]);
+        printf("%d",arr[i]);
     }
-    if(arr[0]>arr[1])
+    else if(arr[n-2]>arr[n-1])
     {
+        printf("%d",arr[n-2])
+    }
+    else if(arr[0]>arr[1]){
         printf("%d",arr[0]);
+        
     }
+    else{
+        printf("-1");
+    }
+   }
 
-    else if (arr[0]<arr[1] ){
-        if(arr[i-1]<arr[i] && arr[i]>arr[i+1])
-        {
-            printf("%d",arr[i]);
-        }
 
-    }
-     else 
-    {
-        printf("%d",arr[n-1]);
-    }
     
 
 
